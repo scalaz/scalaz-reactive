18300235f7f1ba755dcc0caf42172f3cb4f2d0b5
Assen Kolov <assen.kolov@gmail.com>
2018-08-08 19:11:58 +0200

    Future(IO[Noting,(Time,A)]

--- src/main/scala/scalaz/reactive/Event.scala
+++ src/main/scala/scalaz/reactive/Event.scala
@@ -1,11 +1,35 @@
 package scalaz.reactive
+import scalaz.Monoid
+import scalaz.zio.IO
 
-case class Event[+A](value: Future[Reactive[A]]) extends AnyVal {
+case class Event[A](value: Future[Reactive[A]]) extends AnyVal { self =>
+
+  def merge(v: Event[A]): Event[A] = {
+
+    val winner: IO[Nothing, Either[(Time, Reactive[A]), (Time, Reactive[A])]] =
+      self.value.force.map(Left(_)).race(v.value.force.map(Right(_)))
+
+    val io: IO[Nothing, Future[Reactive[A]]] = winner.map(_ match {
+      case Left((t, r)) =>
+         Future((t, Reactive(r.head, r.tail)))
+      case Right((t, r)) =>
+         Future((t, Reactive(r.head, r.tail)))
+    })
+    // FIXME - how to proceed ???
+    ???
+  }
 
   def map[B](f: A => B): Event[B] =
     Event(value.map(_.map(f)))
 
-  // TODO monad
+  implicit def monoidEvent[E, A]: Monoid[Event[A]] =
+    new Monoid[Event[A]] {
+
+      override def zero: Event[A] = Event(Future.Never)
+
+      override def append(e1: Event[A], e2: => Event[A]): Event[A] =
+        e1.merge(e2)
+    }
 
   // TODO monoid
 }
--- src/main/scala/scalaz/reactive/Future.scala
+++ src/main/scala/scalaz/reactive/Future.scala
@@ -1,34 +1,46 @@
 package scalaz.reactive
 
 import scalaz.Scalaz._
-import scalaz.reactive.Time.{ NegInf, PosInf }
-import scalaz.{ Applicative, Functor, Monad, Monoid }
+import scalaz.reactive.Time.{NegInf, PosInf}
+import scalaz.zio.IO
+import scalaz.{Applicative, Functor, Monad, Monoid}
 
-case class Future[+A](time: () => Time, force: () => A) {
+object Types {
+  type Infallible[A] = IO[Nothing, A]
+}
 
-  def + [AA >: A](other: Future[AA]): Future[AA] =
-    Future(
-      () => time().min(other.time()),
-      () => if (time() <= other.time()) force() else other.force()
-    )
+case class Future[+A](force: IO[Nothing, (Time, A)]) {
+
+  def +[AA >: A](other: Future[AA]): Future[AA] =
+    Future(force.flatMap {
+      case (t1, a1) =>
+        other.force.map {
+          case (t2, a2) => if (t1 <= t2) (t1, a1) else (t2, a2)
+        } //There should be IO.ap, is there?
+    })
 
   def map[B](f: A => B): Future[B] =
-    Future(() => time(), () => f(force()))
+    Future(force.map { case (t, a) => (t, f(a)) })
 
   def ap[B](f: => Future[A => B]): Future[B] =
-    Future(() => time().max(f.time()), () => f.force()(force()))
+    Future(force.flatMap {
+      case (t, a) =>
+        f.force
+          .map { case (t2, f) => (t2.max(t), f(a)) } // There should be IO.ap, is there?
+    })
 
-  def flatMap[B](f: A => Future[B]): Future[B] =
-    f(force())
+  def flatMap[B](f: A => Future[B]): Future[B] = {
+    Future(force.flatMap { case (_, a) => f(a).force }) // FIXME - what to do with the times
+  }
 }
 
 object Future extends FutureInstances0 {
 
   def point[A](a: => A): Future[A] =
-    Future(() => NegInf, () => a)
+    Future((NegInf, a))
 
   def apply[A](force: => (Time, A)): Future[A] =
-    Future[A](() => force._1, () => force._2)
+    Future(IO.point(force))
 
   def Never[A]: Future[A] = Future[A](PosInf -> null.asInstanceOf[A])
 }
@@ -73,7 +85,8 @@ trait FutureInstances2 {
       override def point[A](a: => A): Future[A] =
         Future.point(a)
 
-      override def bind[A, B](fa: Future[A])(f: A => Future[B]): Future[B] =
-        f(fa.force())
+      override def bind[A, B](fa: Future[A])(f: A => Future[B]): Future[B] = {
+        Future(fa.force.flatMap { case (_, a) => f(a).force })
+      }
     }
 }
