f7d484f0aa11714400c33fe7722820676ffe8aca
Assen Kolov <assen.kolov@gmail.com>
2018-08-18 10:45:53 +0200

    clean up, readme

--- examples/src/main/scala/scalaz/reactive/examples/Synthesizer.scala
+++ examples/src/main/scala/scalaz/reactive/examples/Synthesizer.scala
@@ -1,17 +1,24 @@
 package scalaz.reactive.examples
 
 import scalaz.reactive._
-import scalaz.zio.{App, IO}
+import scalaz.zio.{ App, IO }
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
+import scalaz._
+import Scalaz._
+import scalaz.reactive.TimeFun.K
+import scalaz.reactive.examples.TwoTickers.{ Tick, ticks }
+
+import scala.util.Random
 
 /**
-  * Example from https://wiki.haskell.org/FRP_explanation_using_reactive-banana
-  */
+ * Example from https://wiki.haskell.org/FRP_explanation_using_reactive-banana
+ */
 object Synthesizer extends App {
 
   type Octave = Int
+
   sealed trait Pitch
   object PA extends Pitch
   object PB extends Pitch
@@ -20,49 +27,49 @@ object Synthesizer extends App {
   object PE extends Pitch
   object PF extends Pitch
   object PG extends Pitch
+
   case class Note(octave: Octave, pitch: Pitch)
 
   val table = Map('a' -> PA, 'b' -> PB, 'g' -> PG)
 
-  def build(eKey: Event[Char]) = {
+  def build(eKey: Event[Char]): Behaviour[Note] = {
     val ePitch = eKey
-        .map(table.get(_))
-        .filter(_.isDefined)
-        .map(_.get) // implement flatten, or filterSome
+      .map(table.get(_))
+      .filter(_.isDefined)
+      .map(_.get) // implement flatten, or filterSome
 
-    val eOctChange : Event[Octave => Octave] = eKey.map { k =>
-      k match {
-        case '+' => Some((x: Octave) => x + 1)
-        case '-' => Some((x: Octave) => x - 1)
-        case _   => None
+    val eOctChange: Event[Octave => Octave] = eKey
+      .map { k =>
+        k match {
+          case '+' => Some((x: Octave) => x + 1)
+          case '-' => Some((x: Octave) => x - 1)
+          case _   => None
+        }
       }
-    }.filter(_.isDefined)
+      .filter(_.isDefined)
       .map(_.get)
 
-    val bOctave: Behaviour[Octave] = ???
-    val bPitch: Behaviour[Pitch] = ???
+    val bOctave: Behaviour[Octave] = Behaviour(Reactive(TimeFun.K(0), ???))
+    val bPitch: Behaviour[Pitch]   = Behaviour(Reactive(TimeFun.K(PA), ???))
 
-    val bNote = bOctave <*> bPitch
-
-
+    (bOctave |@| bPitch) { Note.apply _ }
   }
-
 
   def run(args: List[String]): IO[Nothing, ExitStatus] =
     myAppLogic.attempt.map(_.fold(_ => 1, _ => 0)).map(ExitStatus.ExitNow(_))
 
   case class Tick(name: String)
 
-  def ticks(interval: Duration, name: String): Event[Tick] =
-    Event(IO.point {
-      (Time.now, Reactive(Tick(name), ticks(interval, name).delay(interval)))
-    })
+  def randomChar(lower: Char, upper: Char) =
+    ((Math.random() * (upper - lower)).toInt + lower).toChar
+  def randomChar = randomChar(' ', 128)
 
-  def myAppLogic: IO[Void, Unit] =
-    Sink[Tick, Unit](t => IO.now(println(s"tick ${t.name}")))
-      .sink(
-        ticks(0.2 second, "a")
-          .merge(ticks(0.4 second, "b"))
-      )
+  def eKey(): Event[Char] =
+    Event(IO.point { (Time.now, Reactive(randomChar, eKey())) }) // non FP here
+
+  def myAppLogic: IO[Void, Unit] = {
+    Sink[Note, Unit](n => IO.now(println(s"Note [${n.octave},${n.pitch}]")))
+    ???
+  }
 
 }
