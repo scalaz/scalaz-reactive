0b7d9d286abfc9094aef0976c080fd251afd2190
Assen Kolov <assen.kolov@gmail.com>
2018-08-26 18:51:08 +0300

    extracted laws to a class

--- core/src/test/scala/scalaz/reactive/TimeFunSpec.scala
+++ core/src/test/scala/scalaz/reactive/TimeFunSpec.scala
@@ -1,11 +1,11 @@
 package scalaz.reactive
 
 import org.scalacheck.Gen
-import org.scalacheck.Prop.forAll
 import org.specs2.{ ScalaCheck, Specification }
 import scalaz._
 import scalaz.reactive.Time.T
 import scalaz.reactive.TimeFun._
+import scalaz.reactive.laws.ApplicativeLaws
 
 class TimeFunSpec extends Specification with ScalaCheck with TimeFunInstances {
 
@@ -48,48 +48,6 @@ class TimeFunSpec extends Specification with ScalaCheck with TimeFunInstances {
       l <- Gen.choose[Long](-100, 100)
       t = T(l)
     } yield (tf: TimeFun[Long]) => tf.apply(t)
-
-  // TODO: extract this to a separate class
-  class ApplicativeLaws[F[_], A](
-    applicative: Applicative[F],
-    aGen: Gen[A],
-    faGen: Gen[F[A]],
-    abGen: Gen[A => A],
-    fabGen: Gen[F[A => A]],
-    valueForEqGen: Gen[F[A] => A]) {
-
-    implicit class Equalable(v: F[A]) {
-      def valueForEq(f: F[A] => A) = f(v)
-    }
-
-    // ap(fa)(point(_)) == fa
-    def apIdentityLaw = forAll(faGen, valueForEqGen) { (fa, v) =>
-      applicative
-        .ap(fa)(applicative.point(identity[A](_)))
-        .valueForEq(v) must beEqualTo(fa.valueForEq(v))
-    }
-
-    // ap(point(a))(point(ab)) == point(ab(a))
-    def apHomomorphismLaw = forAll(aGen, abGen) { (a, ab) =>
-      applicative
-        .ap(applicative.point(a))(applicative.point(ab)) must
-        beEqualTo(applicative.point(ab(a)))
-    }
-
-    // ap(point(a))(fab) == ap(fab)(point(_.apply(a)))
-    def apInterchangeLaw = forAll(aGen, fabGen) { (a, fab) =>
-      applicative.ap(applicative.point(a))(fab) must
-        beEqualTo(
-          applicative.ap(fab)(applicative.point((x: A => A) => x.apply(a)))
-        )
-    }
-
-    //map(fa)(ab) == ap(fa)(point(ab))
-    def apDerivedMapLaw = forAll(faGen, abGen, valueForEqGen) { (fa, ab, v) =>
-      applicative.map(fa)(ab).valueForEq(v) must
-        beEqualTo(applicative.ap(fa)(applicative.point(ab)).valueForEq(v))
-    }
-  }
 
   val laws =
     new ApplicativeLaws(Applicative[TimeFun], aGen, faGen, abGen, fabGen, eqGen)
