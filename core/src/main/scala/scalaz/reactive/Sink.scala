02d5fe9a102895a66917f23353dfdd0451c3cd8a
Assen Kolov <assen.kolov@gmail.com>
2018-08-28 10:40:44 +0300

    Eorking example combining and filtering AWT events

--- core/src/main/scala/scalaz/reactive/Sink.scala
+++ core/src/main/scala/scalaz/reactive/Sink.scala
@@ -9,7 +9,7 @@ object Sink {
     sink(r.head).flatMap(_ => sinkE(sink, r.tail))
 
   def sinkE[A, B](sink: Sink[A], e: Event[A]): IO[Void, Unit] =
-    e.value.flatMap { case (_, r) => sinkR(sink, r) }
+    IO.sync(println("sinkE called")).flatMap(_ => e.value.flatMap { case (_, r) => sinkR(sink, r) })
 
   def sinkB[A, B](b: Behaviour[A], tfSink: Sink[TimeFun[A]]): IO[Void, Unit] =
     sinkR(tfSink, b.value)
---
+++ examples/src/main/scala/scalaz/reactive/examples/awt/AwtKeyAndMouse.scala
@@ -0,0 +1,63 @@
+package scalaz.reactive.examples.awt
+
+import java.awt.event._
+import java.awt.{ BorderLayout, Dimension }
+import java.util.EventObject
+
+import javax.swing.{ JButton, JScrollPane, JTextArea, _ }
+import scalaz.reactive.Event
+import scalaz.reactive.Sink.Sink
+import scalaz.zio.{ IO, _ }
+
+object KeyboardAndTime extends App with RTS {
+
+  def run(args: List[String]): IO[Nothing, ExitStatus] =
+    myAppLogic.attempt.map(_.fold(_ => 1, _ => 0)).map(ExitStatus.ExitNow(_))
+
+  def myAppLogic =
+    AwtApp.run(new ThisDemo("AWT Events Demo"))
+}
+
+class ThisDemo(name: String) extends AwtApp(name) {
+
+  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+  val displayArea = new JTextArea
+  buildPane()
+
+  private def buildPane(): Unit = {
+
+    displayArea.setText("Click the mouse or press a lowercase vowel key")
+    val button = new JButton("Clear")
+    button.addActionListener(this)
+    displayArea.addKeyListener(this)
+    displayArea.setEditable(false)
+    val scrollPane = new JScrollPane(displayArea)
+    scrollPane.setPreferredSize(new Dimension(375, 125))
+    getContentPane.add(scrollPane, BorderLayout.CENTER)
+    getContentPane.add(button, BorderLayout.PAGE_END)
+    pack()
+    displayArea.addMouseListener(this)
+    setVisible(true)
+  }
+
+  val sink: Sink[EventObject] = (e: EventObject) =>
+    e match {
+      case e: MouseEvent if e.getID == MouseEvent.MOUSE_CLICKED =>
+        IO.sync(displayArea.append(s"\nClicked at [${e.getX},${e.getY}]"))
+      case e: KeyEvent =>
+        IO.sync(displayArea.append(s"\nKey [${e.getKeyChar}] ${e.getID}]"))
+      case _: ActionEvent =>
+        IO.sync {
+          displayArea.setText("")
+        }
+      case _ => IO.sync(println(s"Ignoring $e"))
+    }
+
+  override def eventsFilter(e: Event[EventObject]): Event[EventObject] =
+    e.filter(
+      e =>
+        !e.isInstanceOf[KeyEvent] || Set('a', 'e', 'i', 'o', 'u')
+          .contains(e.asInstanceOf[KeyEvent].getKeyChar)
+    )
+
+}
